b8ce5a5666023bcb0f21e89f2d147c0ff5ab698e
bartekbb98 <107361500+bartekbb98@users.noreply.github.com>
2022-11-05 18:50:52 +0100

    lab3

--- lab3/skrypt.js
+++ lab3/skrypt.js
@@ -1,23 +1,55 @@
- function odliczanie()
-{
-var dzisiaj = new Date();
+function slist (target) {
+  // (A) SET CSS + GET ALL LIST ITEMS
+  target.classList.add("slist");
+  let items = target.getElementsByTagName("li"), current = null;
 
-var dzien = dzisiaj.getDate();
+  // (B) MAKE ITEMS DRAGGABLE + SORTABLE
+  for (let i of items) {
+    // (B1) ATTACH DRAGGABLE
+    i.draggable = true;
+    
+    // (B2) DRAG START - YELLOW HIGHLIGHT DROPZONES
+    i.ondragstart = (ev) => {
+      current = i;
+      for (let it of items) {
+        if (it != current) { it.classList.add("hint"); }
+      }
+    };
+    
+    // (B3) DRAG ENTER - RED HIGHLIGHT DROPZONE
+    i.ondragenter = (ev) => {
+      if (i != current) { i.classList.add("active"); }
+    };
 
-var miesiac = dzisiaj.getMonth()+1;
+    // (B4) DRAG LEAVE - REMOVE RED HIGHLIGHT
+    i.ondragleave = () => {
+      i.classList.remove("active");
+    };
 
-var rok = dzisiaj.getFullYear();
-
-var godzina = dzisiaj.getHours();
-if(godzina <10) godzina = "0" + godzina;
-var minuta = dzisiaj.getMinutes();
-if(minuta <10) minuta = "0" + minuta;
-var sekunda = dzisiaj.getSeconds();
-if(sekunda <10) sekunda = "0" + sekunda;
-
-
-document.getElementById("zegar").innerHTML = dzien +"/"+miesiac +"/"+rok +" | " + godzina+":"+minuta+":"+sekunda; 
-
-setTimeout("odliczanie()",1000);
-
+    // (B5) DRAG END - REMOVE ALL HIGHLIGHTS
+    i.ondragend = () => { for (let it of items) {
+        it.classList.remove("hint");
+        it.classList.remove("active");
+    }};
+ 
+    // (B6) DRAG OVER - PREVENT THE DEFAULT "DROP", SO WE CAN DO OUR OWN
+    i.ondragover = (evt) => { evt.preventDefault(); };
+ 
+    // (B7) ON DROP - DO SOMETHING
+    i.ondrop = (evt) => {
+      evt.preventDefault();
+      if (i != current) {
+        let currentpos = 0, droppedpos = 0;
+        for (let it=0; it<items.length; it++) {
+          if (current == items[it]) { currentpos = it; }
+          if (i == items[it]) { droppedpos = it; }
+        }
+        if (currentpos < droppedpos) {
+          i.parentNode.insertBefore(current, i.nextSibling);
+        } else {
+          i.parentNode.insertBefore(current, i);
+        }
+      }
+    };
+  }
 }
